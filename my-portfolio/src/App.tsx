c229c24978011301aceec91a2903a7369150e51d
Thirumani Raja <nadar.thirumaniraja@mahindra.com>
2025-07-25 20:20:55 +0530

    my-resume

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -1,12 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import HeroSection from './components/profile'
 import Resume from './components/resume/Resume'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className='App'> 
